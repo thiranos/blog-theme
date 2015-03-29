a0e4f188124cba49b0dfc2909901caaebbc93ac7
Shay Rojansky <roji@roji.org>
2015-03-29 18:50:59 +0000

    Exclude black diary posts from home page

--- index.php
+++ index.php
@@ -16,10 +16,11 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-		<?php if ( have_posts() ) : ?>
+		<?php $query = new WP_Query( 'cat=-2' ); ?>
+		<?php if ( $query->have_posts() ) : ?>
 
 			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
+			<?php while ( $query->have_posts() ) : $query->the_post(); ?>
 
 				<?php
 					/* Include the Post-Format-specific template for the content.
